d2fce25924d6a7db649de045ba039a036f008a63
Salamandar <6552989+Salamandar@users.noreply.github.com>
2023-10-02 13:41:27 +0200

    Initial commit for Woodpecker

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,6 +8,19 @@
 # PERSONAL HELPERS
 #=================================================
 
+_woodpecker_set_forge_config() {
+    FORGE=$(printf '%s\n' "$forge_type" | awk '{ print toupper($0) }')
+    config_forge_type="WOODPECKER_${FORGE}=true"
+    config_forge_client="WOODPECKER_${FORGE}_CLIENT=$forge_client"
+    config_forge_secret="WOODPECKER_${FORGE}_SECRET=$forge_secret"
+
+    if [[ -n "$forge_url" ]]; then
+        config_forge_url="WOODPECKER_${FORGE}_URL=$forge_url"
+    else
+        config_forge_url="# no forge url"
+    fi
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
